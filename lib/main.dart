c336ab415d1ef06a68d55705a0d633cfca9c97ae
josimarminetefera <josimaminete@gmail.com>
2021-04-16 20:17:04 -0300

    Adicionado bibliotecas do firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
+  FirebaseFirestore.instance.collection('teste').add({'teste':'teste'});
 }
 
 class MyApp extends StatelessWidget {
