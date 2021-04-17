3f02e19b9ac517c71f699e027f3e2fa1899b901c
josimarminetefera <josimaminete@gmail.com>
2021-04-16 21:25:16 -0300

    Adicionado tela de base.

---
+++ lib/screens/base/base_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class BaseScreen extends StatelessWidget {
+  final PageController pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      //physics: const NeverScrollableScrollPhysics(),
+      controller: pageController,
+      children: [
+        Container(color: Colors.red),
+        Container(color: Colors.yellow),
+        Container(color: Colors.green),
+      ],
+    );
+  }
+}
