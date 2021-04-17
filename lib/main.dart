7ad5ca2224cfb9c8d8d82f0fc42c4cbc90b104e4
josimarminetefera <josimaminete@gmail.com>
2021-04-16 21:20:54 -0300

    Fazendo testes com Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,44 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual_completa/screens/base/base_screen.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
-  FirebaseFirestore.instance.collection('compilacao').add({'teste':'teste'});
+  _testesComFirebase();
+}
+
+void _testesComFirebase() async {
+  //criando um novo documento
+  FirebaseFirestore.instance.collection('compilacao').add({'nome': 'nome campo'});
+  /*FirebaseFirestore.instance.collection('compilacao').doc('BBL9FFPRNnlGFM1uflqm').set({'nome': 'nome campo'});
+  FirebaseFirestore.instance.collection('compilacao').doc('BBL9FFPRNnlGFM1uflqm').update({'nome': 'nome campo'});
+  DocumentSnapshot documentSnapshot = await FirebaseFirestore.instance.collection('compilacao').doc('BBL9FFPRNnlGFM1uflqm').get();
+  print(documentSnapshot.data()["nome"]);
+  //atualizou o documento ele tem que mudar aqui
+  FirebaseFirestore.instance.collection('compilacao').doc('BBL9FFPRNnlGFM1uflqm').snapshots().listen((event) {
+    print(event.data());
+  });
+  //todos documentos
+  QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection('compilacao').get();
+  //print(querySnapshot.docs);
+  for (DocumentSnapshot documentSnapshot in querySnapshot.docs) {
+    print(documentSnapshot.data());
+  }*/
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Loja Josimar',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Container(),
+      home: BaseScreen(),
     );
   }
 }
