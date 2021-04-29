b8fa3a56d91f5d8dd25e071af7f323ea4947f172
josimarminetefera <josimaminete@gmail.com>
2021-04-29 17:56:55 -0300

    Adicionado validação de campos e do formulário.

---
+++ lib/helpres/validadores.dart
@@ -0,0 +1,4 @@
+bool emailValido(String email) {
+  final RegExp regex = RegExp(r"^(([^<>()[\]\\.,;:\s@\']+(\.[^<>()[\]\\.,;:\s@\']+)*)|(\'.+\'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$");
+  return regex.hasMatch(email);
+}
--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual_completa/helpres/validadores.dart';
 
 class LoginScreen extends StatelessWidget {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController senhaController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,61 +16,73 @@ class LoginScreen extends StatelessWidget {
       body: Center(
         child: Card(
           margin: const EdgeInsets.symmetric(horizontal: 16),
-          child: ListView(
-            padding: const EdgeInsets.all(16),
-            shrinkWrap: true, //menos altura da tela
-            children: [
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  hintText: "E-mail",
-                ),
-                autocorrect: false,
-                validator: (valor) {
-                  return null;
-                },
-              ),
-              const SizedBox(height: 15),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: "Senha",
-                ),
-                autocorrect: false,
-                obscureText: true,
-                validator: (valor) {
-                  if (valor.isEmpty || valor.length < 6) {
-                    return "Senha inválida";
-                  }
-                  return null;
-                },
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  padding: EdgeInsets.zero,
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                    ),
-                    child: const Text("Esqueci minha senha"),
-                    onPressed: () {},
+          child: Form(
+            key: formKey,
+            child: ListView(
+              padding: const EdgeInsets.all(16),
+              shrinkWrap: true, //menos altura da tela
+              children: [
+                TextFormField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    hintText: "E-mail",
                   ),
+                  autocorrect: false,
+                  validator: (valor) {
+                    if (!emailValido(valor)) {
+                      return "Email inválido";
+                    }
+                    return null;
+                  },
                 ),
-              ),
-              SizedBox(
-                height: 50,
-                child: ElevatedButton(
-                  child: Text("Entrar"),
-                  style: ElevatedButton.styleFrom(
-                    primary: Theme.of(context).primaryColor,
-                    textStyle: TextStyle(
-                      fontSize: 18,
+                const SizedBox(height: 10),
+                TextFormField(
+                  controller: senhaController,
+                  decoration: const InputDecoration(
+                    hintText: "Senha",
+                  ),
+                  autocorrect: false,
+                  obscureText: true,
+                  validator: (valor) {
+                    if (valor.isEmpty || valor.length < 6) {
+                      return "Senha inválida";
+                    }
+                    return null;
+                  },
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    padding: EdgeInsets.zero,
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.zero,
+                      ),
+                      child: const Text("Esqueci minha senha"),
+                      onPressed: () {},
                     ),
                   ),
-                  onPressed: () {},
                 ),
-              )
-            ],
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    child: Text("Entrar"),
+                    style: ElevatedButton.styleFrom(
+                      primary: Theme.of(context).primaryColor,
+                      textStyle: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    onPressed: () {
+                      if (formKey.currentState.validate()) {
+                        print(emailController.text);
+                      }
+                    },
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
