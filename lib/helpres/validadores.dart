34845315cffb512d86c7264de2186174f88f49f8
josimarminetefera <josimaminete@gmail.com>
2021-05-03 21:16:01 -0300

    Adicionado funções para validação de campos.

--- lib/helpres/validadores.dart
+++ lib/helpres/validadores.dart
@@ -1,4 +1,12 @@
-bool emailValido(String email) {
+bool validarEmail(String valor) {
   final RegExp regex = RegExp(r"^(([^<>()[\]\\.,;:\s@\']+(\.[^<>()[\]\\.,;:\s@\']+)*)|(\'.+\'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$");
-  return regex.hasMatch(email);
+  return regex.hasMatch(valor);
+}
+
+bool validarNomeCompleto(String valor) {
+  return (valor.trim().split(" ").length <= 1);
+}
+
+bool validarSenha(String valor) {
+  return (valor.length < 6);
 }
