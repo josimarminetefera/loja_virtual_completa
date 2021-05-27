5421a53715829e0c3a5c5fde3eff4947d91b2afd
josimarminetefera <josimaminete@gmail.com>
2021-05-26 23:22:49 -0300

    Adicionado item da lista de produtos.

---
+++ lib/screens/produtos/componentes/produtos_item.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual_completa/models/produto.dart';
+
+class ProdutosItem extends StatelessWidget {
+  final Produto produto;
+
+  const ProdutosItem({this.produto});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+        ),
+        child: Container(
+          padding: EdgeInsets.all(8),
+          height: 100,
+          child: Row(
+            children: [
+              AspectRatio(
+                aspectRatio: 1,
+                child: Image.network(produto.imagens.first),
+              ),
+              SizedBox(width: 16),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly, //ocupa o espaço vertical de 100
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      produto.nome,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 4),
+                      child: Text(
+                        "Apartir de",
+                        style: TextStyle(fontSize: 12, color: Colors.grey[400]),
+                      ),
+                    ),
+                    Text(
+                      "R\$ 19.99",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w800,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/produtos/produtos_screen.dart
+++ lib/screens/produtos/produtos_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual_completa/common/custom_drawer/custom_drawer.dart';
 import 'package:loja_virtual_completa/models/produto_manager.dart';
+import 'package:loja_virtual_completa/screens/produtos/componentes/produtos_item.dart';
 import 'package:provider/provider.dart';
 
 class ProdutosScreen extends StatelessWidget {
@@ -17,10 +18,11 @@ class ProdutosScreen extends StatelessWidget {
       body: Consumer<ProdutoManager>(
         builder: (context, produtoManager, child) {
           return ListView.builder(
+            padding: const EdgeInsets.all(4),
             itemCount: produtoManager.todosProdutos.length,
             itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(produtoManager.todosProdutos[index].nome),
+              return ProdutosItem(
+                produto: produtoManager.todosProdutos[index],
               );
             },
           );
